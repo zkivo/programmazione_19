4d06730b3bf624cddc87ebad474aad2dfca07557
marco.schivo <marco.schivo@outlook.com>
2019-11-26 16:29:55 +0100

    modifica 4 di es03

--- es03/ProgrammaInterattivo.java
+++ es03/ProgrammaInterattivo.java
@@ -6,10 +6,22 @@ public class ProgrammaInterattivo {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        String str = "";
+        String str;
+        String parole_vietate[] = {"alfa", "beta", "gamma", "delta", "epsilon",                            "kappa", "lambda", "sigma", "omega"};
+        int i;
         System.out.print("Digita qualcosa, oppure \"fine\" per terminare\n>>");
-        while (!(str = scanner.next()).equals("fine"))
-            System.out.print("Hai digitato " + str.toUpperCase() + ".\n>>");
+        while (!(str = scanner.next()).equals("fine")) {
+            for (i = 0; i < parole_vietate.length; i++) {
+                if (str.equals(parole_vietate[i])) {
+                    System.out.print("-- Hai Inserito una parola vietata --" + "\n>>");
+                    break;
+                }
+            }
+            if (i == parole_vietate.length) {
+                // non e' stata inserita parola vietata
+                System.out.print("Hai digitato " + str.toUpperCase() + ".\n>>");      
+            }
+        }
         System.out.println("Fine del programma.");
     }
 
