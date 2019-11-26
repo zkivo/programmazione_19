8be7006a38175885a16fee46292d1072a50e6de0
marco.schivo <marco.schivo@outlook.com>
2019-11-26 17:03:08 +0100

    Aggiunto 5 di es03

---
+++ es03/Persona.java
@@ -0,0 +1,27 @@
+package es03;
+
+public class Persona {
+    private String nome;
+    private String cognome;
+    private int eta;
+
+    public Persona setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public Persona setCognome(String cognome) {
+        this.cognome = cognome;
+        return this;
+    }
+
+    public Persona setEta(int eta) {
+        this.eta = eta;
+        return this;
+    }
+
+    public String toString(){
+        return nome + " " + cognome + " " + eta; 
+    }
+
+}
---
+++ es03/Principale.java
@@ -0,0 +1,23 @@
+package es03;
+
+public class Principale {
+
+    public static void main(String args[]) {
+        Persona persona1 = new Persona();
+        Persona persona2 = new Persona();
+        Persona persona3 = new Persona();
+    
+        persona1.setNome("Mario").setCognome("Rossi").setEta(22);
+        persona2.setNome("Giuseppe").setCognome("Russo").setEta(30);
+        persona3 = persona1;
+
+        System.out.println("Persona1: " + persona1.toString() +
+                         "\nPersona2: " + persona2.toString() +
+                         "\nPersona3: " + persona3.toString());
+        
+        persona3.setNome("Luca").setCognome("Bianchi").setEta(25);
+        System.out.println("Persona1: " + persona1.toString() +
+                         "\nPersona2: " + persona2.toString() +
+                         "\nPersona3: " + persona3.toString());
+    }
+}
