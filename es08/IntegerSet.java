5bf05fb29d570742557054fec26e1b3cd2c2a23a
marco.schivo <marco.schivo@outlook.com>
2020-01-26 19:19:14 +0100

    add integerset in 8

---
+++ es08/IntegerSet.java
@@ -0,0 +1,72 @@
+package es08;
+
+public class IntegerSet {
+
+    //MAX_NUM rappresenta il massimo numero rappresentabile + 1
+    private static final int MAX_NUM = 100;
+    private boolean array[];
+
+    public IntegerSet() {
+        array = new boolean[MAX_NUM];
+        for (int i = 0; i < MAX_NUM; i++) array[i] = false;
+    }
+
+    public void insertElement(int elemento) {
+        if (elemento >= 0 && elemento < MAX_NUM) {
+            array[elemento] = true;
+        } else throw new IndexOutOfBoundsException("elemento non compreso " +
+                                                   "tra 0 e " + MAX_NUM);
+    }
+
+    public void deleteElement(int elemento) {
+        if (elemento >= 0 && elemento < MAX_NUM) {
+            array[elemento] = false;
+        } else throw new IndexOutOfBoundsException("elemento non compreso " +
+                                                   "tra 0 e " + MAX_NUM);
+    }
+
+    public IntegerSet unionOfIntegerSet(IntegerSet is) {
+        IntegerSet ret = new IntegerSet();
+        for (int i = 0; i < MAX_NUM; i++) {
+            ret.array[i] = this.array[i] || is.array[i];
+        }
+        return ret;
+    }
+
+    public IntegerSet intersectionOfIntegerSet(IntegerSet is) {
+        IntegerSet ret = new IntegerSet();
+        for (int i = 0; i < MAX_NUM; i++) {
+            ret.array[i] = this.array[i] && is.array[i];
+        }
+        return ret;
+    }
+
+    public String toString() {
+        String ret = "{";
+        for (int i = 0; i < MAX_NUM; i++) {
+            if (array[i]) {
+                ret = ret + i + ", ";
+            }
+        }
+        if (ret.length() == 1) return "---";
+        return ret.subSequence(0, ret.length() - 2) + "}";
+    }
+
+    public static void main(String args[]) {
+        IntegerSet i1 = new IntegerSet();
+        IntegerSet i2 = new IntegerSet();
+
+        System.out.println(i1);
+        i1.insertElement(0);
+        i1.insertElement(100);
+        i1.insertElement(47);
+        i2.insertElement(2);
+        i2.insertElement(32);
+        i2.insertElement(47);
+        System.out.println("i1: " + i1);
+        System.out.println("i2: " + i2);
+        System.out.println("union: " + i1.unionOfIntegerSet(i2));
+        System.out.println("intersection: " + i1.intersectionOfIntegerSet(i2));
+    }
+
+}
