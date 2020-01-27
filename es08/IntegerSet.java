831ceb8e946f797d8476fc69ba3cfce63de99f3c
marco.schivo <marco.schivo@outlook.com>
2020-01-27 10:07:36 +0100

    add 4 di 8

--- es08/IntegerSet.java
+++ es08/IntegerSet.java
@@ -1,5 +1,7 @@
 package es08;
 
+import java.util.Scanner;
+
 public class IntegerSet {
 
     //MAX_NUM rappresenta il massimo numero rappresentabile + 1
@@ -56,17 +58,64 @@ public class IntegerSet {
         IntegerSet i1 = new IntegerSet();
         IntegerSet i2 = new IntegerSet();
 
-        System.out.println(i1);
-        i1.insertElement(0);
-        i1.insertElement(100);
-        i1.insertElement(47);
-        i2.insertElement(2);
-        i2.insertElement(32);
-        i2.insertElement(47);
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        IntegerSet rf_is;
+        do {
+            System.out.print("Selezionare IntegerSet 1 o 2? (-1 per uscire)\n>>");
+            input = scanner.nextInt();
+            if (input == 1) rf_is = i1;
+            else if (input == 2) rf_is = i2;
+            else if (input == -1) break;
+            else {
+                System.out.println("- Errore - Puoi inserire solo \'1' o '2'");
+                continue;
+            }
+            System.out.print("Cosa vuoi fare?\n(I)nserire nuovo elemento\n" + 
+                             "(E)liminare elemento\n" + 
+                             "(S)tampare l'IntegerSet\n" + 
+                             "(T)orna indietro\n>>");
+            switch (scanner.next().toUpperCase()) {
+                case "I":
+                    try {
+                        System.out.print("Inserire numero\n>>");
+                        rf_is.insertElement(scanner.nextInt());
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(e.getMessage());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                break;
+
+                case "E":
+                    try {
+                        System.out.print("Inserire numero\n>>");
+                        rf_is.deleteElement(scanner.nextInt());
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(e.getMessage());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                break;
+
+                case "S":
+                    System.out.println(rf_is);
+                break;
+
+                case "T":
+                    continue;
+            
+                default:
+                    System.out.println("Input non valido");
+                break;
+            }
+        } while (true);
+
         System.out.println("i1: " + i1);
         System.out.println("i2: " + i2);
         System.out.println("union: " + i1.unionOfIntegerSet(i2));
         System.out.println("intersection: " + i1.intersectionOfIntegerSet(i2));
+        scanner.close();
     }
 
 }
